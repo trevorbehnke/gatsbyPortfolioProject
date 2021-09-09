eab79d2f233bb232af8b9d453f74da863c504db1
trevor <me@trevorbehnke.com>
2021-09-08 20:15:34 -0700

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,39 @@
 import React from "react"
+import { Link } from "gatsby"
+// import { graphql } from "gatsby"
+import Layout from "../components/Layout"
+import * as styles from "../styles/home.module.css"
 
-export default function Home() {
-  return <div>Hello world!</div>
+export default function Home({ data }) {
+  // console.log(data)
+  // const { title, description } = data.site.siteMetadata
+  return (
+    <Layout>
+      <section className={styles.header}>
+        <div>
+          <h2>Design</h2>
+          <h3>Develop & Deploy</h3>
+          <p>UX designer & web developer</p>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
+        </div>
+        <img src="/banner.png" alt="site banner" style={{ maxWidth: "100%" }} />
+        {/* <p>
+          {title} - {description}
+        </p> */}
+      </section>
+    </Layout>
+  )
 }
+
+// export const query = graphql`
+//   query SiteInfo {
+//     site {
+//       siteMetadata {
+//         description
+//         title
+//       }
+//     }
+//   }
+// `
